29ae4451ee17a562ce123b424f2aefde275acd8d
mysticis <eshokhlah@gmail.com>
2020-05-13 19:33:46 +0100

    implement patientor backend - steps 1 and 2

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/api/ping", (_req, res) => {
+  res.send(`connection established...!`);
+});
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
