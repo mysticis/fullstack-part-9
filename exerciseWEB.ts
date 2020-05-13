8678c8c0a13e58b00a6fbd4c3c042d6975be4904
mysticis <eshokhlah@gmail.com>
2020-05-13 18:32:21 +0100

    final commit - update .eslintrc file

--- exerciseWEB.ts
+++ exerciseWEB.ts
@@ -1,25 +1,25 @@
-import express from "express"
-import bodyParser from "body-parser"
-import { calculateResultsWeb } from "./exerciseFunction"
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+import express from "express";
+import bodyParser from "body-parser";
+import { calculateResultsWeb } from "./exerciseFunction";
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (_req, res) => {
-  res.send(`Hello from port 5000`)
-})
+  res.send(`Hello from port 5000`);
+});
 app.post("/exercises", (req, res) => {
-  const body = req.body
-  const daily_exercises = body.daily_exercises
-  const target = body.target
-  const data = calculateResultsWeb(target, daily_exercises)
+  const body = req.body;
+  const daily_exercises = body.daily_exercises;
+  const target = body.target;
+  const data = calculateResultsWeb(target, daily_exercises);
   if (data) {
-    res.send(calculateResultsWeb(target, daily_exercises))
+    res.send(calculateResultsWeb(target, daily_exercises));
   } else {
-    res.status(400).send({ error: `malformed parameters` })
+    res.status(400).json({ error: `malformed parameters` });
   }
-})
+});
 
-const PORT = 5000
+const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
